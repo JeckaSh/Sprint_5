973c469eabfe1b3755f7dcbd406ba510729e2b4d
JeckaSh <jeckashevelkov@icloud.com>
2026-05-10 15:12:47 +0300

    add 4-th test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -26,13 +26,6 @@ def test_user_registration_success(driver, url, random_email):
 
     driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
 
-    throw_advice_button = WebDriverWait(driver, 5).until(
-        expected_conditions.visibility_of_element_located(THROW_ADVICE)
-    )
-
-    # проверяем, что отображется кнопка "разместить объявление"
-    assert throw_advice_button.is_displayed()
-
     username = WebDriverWait(driver, 5).until(
         expected_conditions.visibility_of_element_located(USERNAME)
     )
@@ -158,3 +151,72 @@ def test_user_registration_with_existing_account(driver, url, random_email):
 
     # проверяем, что отображается красная рамка на полях ввода
     assert error_fileds.is_displayed()
+
+
+def test_user_log_in_success(driver, url, random_email):
+    driver.get(url)
+
+    driver.find_element(*AUTH_BUTTON).click()
+
+    WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(LOG_IN_FORM)
+    )
+
+    driver.find_element(*NO_ACCOUNT_BUTTON).click()
+
+    WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(SIGN_IN_FORM)
+    )
+
+    # регистируем пользователя
+    driver.find_element(*EMAIL_FIELD).send_keys(random_email)
+    driver.find_element(*PASSWORD_FIELD).send_keys("password")
+    driver.find_element(*REPEAT_PASSWORD_FIELD).send_keys("password")
+
+    driver.find_element(*CREATE_ACCOUNT_BUTTON).click()
+
+    username = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(USERNAME)
+    )
+
+    user_avatar = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(AVATAR_BUTTON)
+    )
+
+    # проверяем, что отображется имя и аватар пользователя
+    assert username.is_displayed()
+    assert user_avatar.is_displayed()
+
+    # выходим из созданного профиля
+    driver.find_element(*LOGOUT_BUTTON).click()
+
+    auth_button = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(AUTH_BUTTON)
+    )
+
+    # проверяем, что снова отображается кнопка "вход и регистрации"
+    assert auth_button.is_displayed()
+
+    driver.find_element(*AUTH_BUTTON).click()
+
+    WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(LOG_IN_FORM)
+    )
+
+    # авторизуем пользователя с ранее созданными данными
+    driver.find_element(*EMAIL_FIELD).send_keys(random_email)
+    driver.find_element(*PASSWORD_FIELD).send_keys("password")
+
+    driver.find_element(*LOG_IN_BUTTON).click()
+
+    username = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(USERNAME)
+    )
+
+    user_avatar = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(AVATAR_BUTTON)
+    )
+
+    # проверяем, что отображется имя и аватар пользователя
+    assert username.is_displayed()
+    assert user_avatar.is_displayed()
