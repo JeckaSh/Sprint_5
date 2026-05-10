53e4535fbbe663594a8f6235ecb90ccb68bc1b53
JeckaSh <jeckashevelkov@icloud.com>
2026-05-10 15:42:25 +0300

    add 5-th test

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -30,15 +30,12 @@ def test_user_registration_success(driver, url, random_email, password):
         expected_conditions.visibility_of_element_located(USERNAME)
     )
 
-    # проверяем, что отображется имя пользователя
-    assert username.is_displayed()
-
     user_avatar = WebDriverWait(driver, 5).until(
         expected_conditions.visibility_of_element_located(AVATAR_BUTTON)
     )
 
     # проверяем, что отображается аватар пользователя
-    assert user_avatar.is_displayed()
+    assert user_avatar.is_displayed() and username.is_displayed()
 
 
 def test_user_registration_with_invalid_email_format(driver, url, email_without_mask):
@@ -66,13 +63,10 @@ def test_user_registration_with_invalid_email_format(driver, url, email_without_
 
     error_message = driver.find_element(*ERROR_TEXT)
 
-    # проверяем, что отображается сообщение об ошибке
-    assert error_message.is_displayed()
-
     error_fileds = driver.find_element(*ERROR_FIELDS)
 
     # проверяем, что отображается красная рамка на полях ввода
-    assert error_fileds.is_displayed()
+    assert error_fileds.is_displayed() and error_message.is_displayed()
 
 
 def test_user_registration_with_existing_account(driver, url, test_user, password):
@@ -103,13 +97,10 @@ def test_user_registration_with_existing_account(driver, url, test_user, passwor
 
     error_message = driver.find_element(*ERROR_TEXT)
 
-    # проверяем, что отображается сообщение об ошибке
-    assert error_message.is_displayed()
-
     error_fileds = driver.find_element(*ERROR_FIELDS)
 
     # проверяем, что отображается красная рамка на полях ввода
-    assert error_fileds.is_displayed()
+    assert error_fileds.is_displayed() and error_message.is_displayed()
 
 
 def test_user_log_in_success(driver, url, test_user, password):
@@ -136,5 +127,40 @@ def test_user_log_in_success(driver, url, test_user, password):
     )
 
     # проверяем, что отображется имя и аватар пользователя
-    assert username.is_displayed()
-    assert user_avatar.is_displayed()
+    assert username.is_displayed() and user_avatar.is_displayed()
+
+
+def test_user_log_out_success(driver, url, test_user, password):
+    driver.get(url)
+
+    driver.find_element(*AUTH_BUTTON).click()
+
+    WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(LOG_IN_FORM)
+    )
+
+    # авторизуем пользователя с ранее созданными данными
+    driver.find_element(*EMAIL_FIELD).send_keys(test_user)
+    driver.find_element(*PASSWORD_FIELD).send_keys(password)
+
+    driver.find_element(*LOG_IN_BUTTON).click()
+
+    username = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(USERNAME)
+    )
+
+    user_avatar = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(AVATAR_BUTTON)
+    )
+
+    # проверяем, что отображется имя и аватар пользователя
+    assert username.is_displayed() and user_avatar.is_displayed()
+
+    driver.find_element(*LOGOUT_BUTTON).click()
+
+    log_in_button = WebDriverWait(driver, 5).until(
+        expected_conditions.visibility_of_element_located(AUTH_BUTTON)
+    )
+
+    # проверям что кнопка "войти или зарегистрироваться" снова отображается
+    assert log_in_button.is_displayed()
